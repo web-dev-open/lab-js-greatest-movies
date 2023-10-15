4a2f219d7a6931033c93602b33a38a46ece9433c
hackwell <hackwell.4216@gmail.com>
2023-10-15 08:36:54 +0000

    complete iteration 3

--- src/movies.js
+++ src/movies.js
@@ -2,26 +2,40 @@
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
 function getAllDirectors(moviesArray) {
-  const directors = moviesArray.map(movie => {
+  const directors = moviesArray.map((movie) => {
     return movie.director;
   });
 
   return directors.filter((value, index, arr) => arr.indexOf(value) === index);
 }
 
-
-
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 function howManyMovies(moviesArray) {
-  const findMovieAuthor = moviesArray.filter(movie => {
-    return movie.director === 'Steven Spielberg' && movie.genre.includes('Drama');
+  const findMovieAuthor = moviesArray.filter((movie) => {
+    return (
+      movie.director === 'Steven Spielberg' && movie.genre.includes('Drama')
+    );
   });
 
   return findMovieAuthor.length;
 }
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
+function scoresAverage(moviesArray) {
+  if (moviesArray.length === 0) return 0;
+  else {
+    const validMovies = moviesArray.filter(
+      (movie) => typeof movie.score === 'number' && !isNaN(movie.score)
+    );
+
+    const totalScores = validMovies.reduce((accumulator, movie) => {
+      return (accumulator += movie.score);
+    }, 0);
+
+    const averageScore = totalScores / moviesArray.length;
+    return parseFloat(averageScore.toFixed(2));
+  }
+}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 function dramaMoviesScore(moviesArray) {}
@@ -38,8 +52,6 @@ function turnHoursToMinutes(moviesArray) {}
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
 function bestYearAvg(moviesArray) {}
 
-
-
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
 if (typeof module !== 'undefined') {
@@ -51,6 +63,6 @@ if (typeof module !== 'undefined') {
     orderByYear,
     orderAlphabetically,
     turnHoursToMinutes,
-    bestYearAvg,
+    bestYearAvg
   };
 }
