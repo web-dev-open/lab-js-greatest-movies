dfb02c14a6dbd34cf3309f6605d7958a8b4e0f7a
ChathuraHasanga44 <s92067130@ousl.lk>
2023-11-22 18:59:09 +0530

    Solved lab

--- src/movies.js
+++ src/movies.js
@@ -8,16 +8,13 @@ function getAllDirectors(moviesArray) {
 console.log(getAllDirectors(movies));
 
 
-// let directors = movies.map((movies) => movies.director);
-// console.log(directors);
-
-// let uniqueArray = movies.filter((string, index, self) => {
-//   return self.indexOf(string) === index;
-// });
-// console.log(uniqueArray);
-
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {}
+
+function howManyMovies(moviesArray) {
+  return moviesArray.filter(movie => movie.director === "Steven Spielberg");
+}
+console.log(howManyMovies(movies));
+
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
 function scoresAverage(moviesArray) {}
