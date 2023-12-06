d68913cba010a245d6207584e0729d6f57a020fa
Gabriella Neri <99314986+GabN-hub@users.noreply.github.com>
2023-12-06 15:13:52 +0800

    finished iteration 1 and bonus

--- src/movies.js
+++ src/movies.js
@@ -1,7 +1,18 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(moviesArray) {}
+
+const moviesArray = require('../src/data');
+function getAllDirectors(moviesArray) {
+  let allDirectors = moviesArray.map((movie) => movie.director);
+  let uniqueDirectors = allDirectors.filter((director, index, self) => {
+    self.indexOf(director) === index;
+    return self.indexOf(director) === index;
+  });
+  return uniqueDirectors;
+}
+const directors = getAllDirectors(moviesArray);
+console.log(directors);
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 function howManyMovies(moviesArray) {}
@@ -24,8 +35,6 @@ function turnHoursToMinutes(moviesArray) {}
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
 function bestYearAvg(moviesArray) {}
 
-
-
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
 if (typeof module !== 'undefined') {
@@ -37,6 +46,6 @@ if (typeof module !== 'undefined') {
     orderByYear,
     orderAlphabetically,
     turnHoursToMinutes,
-    bestYearAvg,
+    bestYearAvg
   };
 }
